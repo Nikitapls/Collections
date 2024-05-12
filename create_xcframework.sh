e48856da8fe348f5941929efa52a5e2a65b41547
Nikitapls <nikitanikpls@gmail.com>
2024-05-12 15:29:17 +0300

    create frameworks using function, cleanup frameworks build results

--- create_xcframework.sh
+++ create_xcframework.sh
@@ -4,13 +4,16 @@ SCRIPT_FOLDER=$(dirname -- "$0")
 
 cd "$SCRIPT_FOLDER" || exit
 
+readonly ARCHIVE_PATH="archives"
+readonly FRAMEWORK_NAME="Collections"
+
 archive_framework() {
   platform=$1
   xcodebuild archive \
     -project ./Collections/Collections.xcodeproj \
     -scheme Collections \
     -destination "generic/platform=$platform" \
-    -archivePath "archives/Collections-$platform" \
+    -archivePath "$ARCHIVE_PATH/$FRAMEWORK_NAME-$platform" \
     SKIP_INSTALL=NO \
     BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 }
@@ -18,8 +21,11 @@ archive_framework() {
 archive_framework iOS
 archive_framework macOS
 
-# Create the XCFramework
 xcodebuild -create-xcframework \
-    -archive archives/Collections-iOS.xcarchive -framework Collections.framework \
-    -archive archives/Collections-macOS.xcarchive -framework Collections.framework \
-    -output "./Collections.xcframework"
+    -archive "$ARCHIVE_PATH/$FRAMEWORK_NAME-iOS.xcarchive" \
+    -framework $FRAMEWORK_NAME.framework \
+    -archive "$ARCHIVE_PATH/$FRAMEWORK_NAME-macOS.xcarchive" \
+    -framework $FRAMEWORK_NAME.framework \
+    -output "./builds/$FRAMEWORK_NAME.xcframework"
+
+rm -rf "$ARCHIVE_PATH/"
