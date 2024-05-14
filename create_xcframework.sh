29ab2d460ea4b2cf197a0778495ae361154a0b73
Nikitapls <nikitanikpls@gmail.com>
2024-05-14 13:53:16 +0300

    use bracets for constants

--- create_xcframework.sh
+++ create_xcframework.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env sh
 
-readonly SCRIPT_FOLDER=$(dirname -- "$0")
+readonly SCRIPT_FOLDER=$(dirname -- "${0}")
 
-cd "$SCRIPT_FOLDER" || exit
+cd "${SCRIPT_FOLDER}" || exit
 
 readonly ARCHIVE_FOLDER="archives"
 readonly FRAMEWORK_NAME="Collections"
 
 archive_framework() {
-  platform=$1
+  platform="${1}"
   xcodebuild archive \
     -project ./Collections/Collections.xcodeproj \
     -scheme Collections \
-    -destination "generic/platform=$platform" \
-    -archivePath "$ARCHIVE_FOLDER/$FRAMEWORK_NAME-$platform"
+    -destination "generic/platform=${platform}" \
+    -archivePath "${ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-${platform}"
 }
 
 archive_framework iOS
@@ -22,10 +22,10 @@ archive_framework macOS
 rm -rf "builds/"
 
 xcodebuild -create-xcframework \
-    -archive "$ARCHIVE_FOLDER/$FRAMEWORK_NAME-iOS.xcarchive" \
-    -framework $FRAMEWORK_NAME.framework \
-    -archive "$ARCHIVE_FOLDER/$FRAMEWORK_NAME-macOS.xcarchive" \
-    -framework $FRAMEWORK_NAME.framework \
-    -output "builds/$FRAMEWORK_NAME.xcframework"
+    -archive "${ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-iOS.xcarchive" \
+    -framework ${FRAMEWORK_NAME}.framework \
+    -archive "${ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-macOS.xcarchive" \
+    -framework ${FRAMEWORK_NAME}.framework \
+    -output "builds/${FRAMEWORK_NAME}.xcframework"
 
-rm -rf "$ARCHIVE_FOLDER/"
+rm -rf "${ARCHIVE_FOLDER}/"
