68e3f0be6e20d543046a757e002e798722c79b21
Nikitapls <nikitanikpls@gmail.com>
2024-06-27 19:29:28 +0300

    fix linter issue using swiftformat

--- Collections/CollectionsTests/StackStatistics/StackStatisticsMinimumElementTests.swift
+++ Collections/CollectionsTests/StackStatistics/StackStatisticsMinimumElementTests.swift
@@ -58,7 +58,8 @@ final class StackStatisticsMinimumElementTests: XCTestCase {
         // When
         var minimumElements = [stack.minimumElement()]
         while stack.pop() != nil,
-              let nextMinimumElement = stack.minimumElement() {
+              let nextMinimumElement = stack.minimumElement()
+        {
             minimumElements.append(nextMinimumElement)
         }
 
